9710afe471a3ef22ef0030d2225e50af10e4c83e
Monica Velasquez Samame <samameyuli@gmail.com>
2014-11-26 16:08:16 -0500

    Nuestro primer ejemplo con PHP

---
+++ ejmplosencillo/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Prueba de PHP</title>
+    </head>
+    <body>
+
+<?php
+   echo " Curso Parte de PHP<br>";
+   
+   for($i=0;$i<10;$i++)
+   {
+      echo "Linea ".$i."<br>";
+     }
+?> 
+ </body>
+</html>
